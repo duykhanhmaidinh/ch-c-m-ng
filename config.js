a906b7d1051d7eb801b390066b67d157c9e360e8
duykhanhmaidinh <101075894+duykhanhmaidinh@users.noreply.github.com>
2022-03-07 08:58:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'lớp 11c10';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100018395289027';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'lớp 11c10';
+const contentCard = 'Nhân ngày 8-3, chúc cô và các bạn nữ 11c10 zui zẻ, hạnh phúc bên gia đình của mình và đặc biệt hơn hết là mong cô nhanh hết bệnh để đi dạy cho chúng em❤️.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
